11599409e3758470ed9ff7bc3b8694c0a544b9ec
Shubh Tamrakar <166866724+Shubh-Tamrakar@users.noreply.github.com>
2026-08-10 01:25:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Bellman Ford/bellman-ford.java
@@ -0,0 +1,52 @@
+class Solution {
+
+    public ArrayList<Integer> bellmanFord(int V, int[][] edges, int src) {
+
+        int[] dist = new int[V];
+
+        Arrays.fill(dist, 100000000);
+
+        dist[src] = 0;
+
+        // V - 1 times relaxation
+        for (int i = 1; i <= V - 1; i++) {
+
+            for (int[] edge : edges) {
+
+                int u = edge[0];
+                int v = edge[1];
+                int wt = edge[2];
+
+                if (dist[u] != 100000000 &&
+                    dist[u] + wt < dist[v]) {
+
+                    dist[v] = dist[u] + wt;
+                }
+            }
+        }
+
+   
+        for (int[] edge : edges) {
+
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+
+            if (dist[u] != 100000000 &&
+                dist[u] + wt < dist[v]) {
+
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(-1);
+                return list;
+            }
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < V; i++) {
+            list.add(dist[i]);
+        }
+
+        return list;
+    }
+}
