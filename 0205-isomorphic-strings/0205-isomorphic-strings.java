ed930adcd934f8c4f27ada1edd383bbaabda1151
Shubh Tamrakar <166866724+Shubh-Tamrakar@users.noreply.github.com>
2026-08-15 19:51:16 +0530

    Time: 11 ms (68.04%), Space: 44.2 MB (15.63%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,17 +1,36 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length() != t.length()) return false;
-        int freq1[] = new int[256];
-        int freq2[] = new int[256];
-       
 
-          for(int i =0;i<s.length() ;i++) {
-            if( freq1[s.charAt(i)] !=  freq2[t.charAt(i)]) {
-                return false;
-            }
-            freq1[s.charAt(i) ] = i+1;
-            freq2[t.charAt(i) ] = i+1;
+        if (s.length() != t.length()) {
+            return false;
         }
+
+        HashMap<Character, Character> map = new HashMap<>();
+       HashSet<Character> used = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char a = s.charAt(i);
+            char b = t.charAt(i);
+
+            // a already mapped
+            if (map.containsKey(a)) {
+                if (map.get(a) != b) {
+                    return false;
+                }
+            }
+            // a is new
+            else {
+                // b is already mapped to some other character
+               if(used.contains(b)) {
+                return false;
+               }
+
+                map.put(a, b);
+                used.add(b);
+            }
+        }
+
         return true;
     }
 }
