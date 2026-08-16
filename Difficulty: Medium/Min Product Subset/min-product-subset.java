1e5b39a603d0cb1927ba011b2efe2f164aa3b827
Shubh Tamrakar <166866724+Shubh-Tamrakar@users.noreply.github.com>
2026-08-16 23:43:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Product Subset/min-product-subset.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static int minProd(int[] arr) {
+
+        int n = arr.length;
+        int ans = Integer.MAX_VALUE;
+
+        for (int mask = 1; mask < (1 << n); mask++) {
+
+            int product = 1;
+
+            for (int i = 0; i < n; i++) {
+
+                if ((mask & (1 << i)) != 0) {
+                    product *= arr[i];
+                }
+            }
+
+            ans = Math.min(ans, product);
+        }
+
+        return ans;
+    }
+}
