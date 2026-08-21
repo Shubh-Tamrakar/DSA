7659d990868d8845dcd94dd52d15dff65d034a60
Shubh Tamrakar <166866724+Shubh-Tamrakar@users.noreply.github.com>
2026-08-22 00:27:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Egg Dropping Puzzle/egg-dropping-puzzle.java
@@ -0,0 +1,19 @@
+class Solution {
+     static int eggDrop(int n, int k) {
+
+         int[] dp = new int[n + 1];
+
+         int moves = 0;
+
+         while (dp[n] < k) {
+
+             moves++;
+
+             for (int eggs = n; eggs >= 1; eggs--) {
+                 dp[eggs] = dp[eggs] + dp[eggs - 1] + 1;
+             }
+         }
+
+         return moves;
+     }
+ }
