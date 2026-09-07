960a326c197895fd9812a3ca61fd19e6e783deca
Shubh Tamrakar <166866724+Shubh-Tamrakar@users.noreply.github.com>
2026-09-07 16:09:15 +0530

    Time: 3 ms (89.36%), Space: 43.3 MB (70.21%) - LeetHub

---
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int distinctSubseqII(String s) {
+
+        int MOD = 1_000_000_007;
+
+        long total = 0;
+
+        // dp[c] = number of distinct subsequences
+        // ending with character c
+        long[] dp = new long[26];
+
+        for (char ch : s.toCharArray()) {
+
+            int index = ch - 'a';
+
+            long newSubsequences = (total + 1) % MOD;
+
+            // Remove old subsequences ending with same character
+            total = (total + newSubsequences - dp[index] + MOD) % MOD;
+
+            dp[index] = newSubsequences;
+        }
+
+        return (int) total;
+    }
+}
