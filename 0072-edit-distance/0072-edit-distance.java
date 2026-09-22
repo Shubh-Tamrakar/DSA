0bdff2b63380be2ad7a6cb8764ba946a515e31a9
Shubh Tamrakar <166866724+Shubh-Tamrakar@users.noreply.github.com>
2026-09-22 23:46:35 +0530

    Time: 5 ms (66.1%), Space: 47.2 MB (52.35%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,59 +1,44 @@
 class Solution {
-
     public int minDistance(String word1, String word2) {
 
-        int n = word1.length();
-        int m = word2.length();
+        int m = word1.length();
+        int n = word2.length();
 
-        int[][] dp = new int[n + 1][m + 1];
+        int[][] dp = new int[m + 1][n + 1];
 
-        for (int i = 0; i <= n; i++) {
-            Arrays.fill(dp[i], -1);
+        // word1 -> empty
+        // Delete all characters
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
         }
 
-        return solve(n, m, word1, word2, dp);
-    }
-
-    static int solve(int i, int j,
-                     String word1, String word2,
-                     int[][] dp) {
-
-        // Word1 empty
-        if (i == 0) {
-            return j;
+        // empty -> word2
+        // Insert all characters
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
         }
 
-        // Word2 empty
-        if (j == 0) {
-            return i;
+        for (int i = 1; i <= m; i++) {
+
+            for (int j = 1; j <= n; j++) {
+
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+
+                    dp[i][j] = dp[i - 1][j - 1];
+
+                } else {
+
+                    dp[i][j] = 1 + Math.min(
+                        dp[i - 1][j - 1],              // Replace
+                        Math.min(
+                            dp[i - 1][j],              // Delete
+                            dp[i][j - 1]               // Insert
+                        )
+                    );
+                }
+            }
         }
 
-        // Already calculated
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        // Characters are same
-        if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-
-            return dp[i][j] =
-                    solve(i - 1, j - 1, word1, word2, dp);
-        }
-
-        // Insert
-        int insert =
-                1 + solve(i, j - 1, word1, word2, dp);
-
-        // Delete
-        int delete =
-                1 + solve(i - 1, j, word1, word2, dp);
-
-        // Replace
-        int replace =
-                1 + solve(i - 1, j - 1, word1, word2, dp);
-
-        return dp[i][j] =
-                Math.min(insert,
-                Math.min(delete, replace));
+        return dp[m][n];
     }
 }
